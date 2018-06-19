c19caa30fbfc5acfc92de21e9f991166a32ba476
Sophie Kirschner <sophie@thehumangeo.com>
2018-06-19 16:54:37 +0300

    Improvements to how paths are converted to strings

--- src/validation-path.js
+++ src/validation-path.js
@@ -11,28 +11,33 @@ ValidationPath.prototype.getNextPath = function(attribute){
 };
 
 ValidationPath.prototype.toString = function(){
-    if(this.attribute === null || this.attribute === undefined || (
-        typeof(this.attribute) === "string" && this.attribute.length <= 0
+    if(
+        this.attribute === null ||
+        this.attribute === undefined || (
+        this.attribute === ""
     )){
         return this.parent ? this.parent.toString() : "";
     }else if(typeof(this.attribute) === "number"){
-        return (this.parent || "") + "[" + this.attribute + "]";
+        return `${this.parent || ""}[${this.attribute}]`;
+    }else if(!this.parent ||
+        this.parent.attribute === null ||
+        this.parent.attribute === undefined ||
+        this.parent.attribute === ""
+    ){
+        return `["${this.attribute}"]`;
     }
-    let looksLikeIdentifier = true;
     const attribute = String(this.attribute);
-    if(looksLikeIdentifier){
-        looksLikeIdentifier = ValidationPath.alpha.indexOf(attribute[0]) >= 0;
-    }
+    let looksLikeIdentifier = ValidationPath.alpha.indexOf(attribute[0]) >= 0;
     for(let i = 1; looksLikeIdentifier && i < attribute.length; i++){
-        if(!ValidationPath.alphaNumeric.indexOf(attribute[0]) >= 0){
+        if(ValidationPath.alphaNumeric.indexOf(attribute[i]) < 0){
             looksLikeIdentifier = false;
             break;
         }
     }
     if(looksLikeIdentifier){
-        return this.parent ? (this.parent + "." + attribute) : attribute;
+        return `${this.parent}.${this.attribute}`;
     }else{
-        return (this.parent || "") + "[\"" + attribute + "\"]";
+        return `${this.parent}["${this.attribute}"]`;
     }
 };
 
