22df848bf4edb5ba14c2e06e373d567421822bc6
Sophie Kirschner <sophie@thehumangeo.com>
2018-09-14 09:38:30 +0300

    Change "email" validator behavior
    - Don't lowercase (part before the domain can be case-sensitive)
    - Strip whitespace (autocomplete on mobile often adds a space at the end of form inputs)

--- src/default-validators.js
+++ src/default-validators.js
@@ -387,7 +387,7 @@ const emailAddressValidator = Validator.add({
         if(typeof(value) !== "string"){
             throw new ValueError("Value isn't a string.");
         }
-        const email = String(value).toLowerCase();
+        const email = String(value).trim();
         if(email.indexOf("@") < 0){
             throw new ValueError("Value does not contain a '@' character.");
         }
